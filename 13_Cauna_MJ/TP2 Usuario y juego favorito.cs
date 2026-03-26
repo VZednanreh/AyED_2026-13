b047d5e2d9aef54722a41251dfa6c4427674342c
VZednanreh <valencauna@gmail.com>
2026-03-26 16:27:32 -0300

    Add files via upload

---
+++ 13_Cauna_MJ/TP2 Usuario y juego favorito.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String consola;
+            String juegoFav;
+            String nombre;
+            Console.WriteLine("Hola usuario");
+            Console.Write("Como te llamas: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Que onda " + nombre);
+            Console.Write("¿Cual es tu juego favorito? : ");
+            juegoFav = Console.ReadLine();
+            Console.Write("¿En que consola se juega ese juego? : ");
+            consola = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre + " tu juego favorito es " + juegoFav + " y se juega en " + consola);
+        }
+    }
+}
