3b72cc112b4063595162ce05452ab8d30ad6ffaf
VZednanreh <valencauna@gmail.com>
2026-03-26 16:54:06 -0300

    Add files via upload

---
+++ 13_Cauna_MJ/TP3 Calculo de edad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que onda usuario.");
+            Console.Write("¿En que año naciste? : ");
+            int Año = Convert.ToInt32(Console.ReadLine());
+            int Edad = (2026 - Año);
+            Console.WriteLine("Entonces si naciste en " + Año + ", Tendrias que tener " + Edad + " años.");
+        }
+    }
+}
