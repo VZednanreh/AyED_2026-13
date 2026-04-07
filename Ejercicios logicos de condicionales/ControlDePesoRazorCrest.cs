6563f59a382cb404dd716be4bce520b401d16254
VZednanreh <valencauna@gmail.com>
2026-04-07 17:05:36 -0300

    Add files via upload

---
+++ Ejercicios logicos de condicionales/ControlDePesoRazorCrest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double PesoNave;
+            Double Diferencia;
+            Console.Write("Ingrese el peso de la nave capitan: ");
+            PesoNave = Convert.ToDouble(Console.ReadLine());
+            if (PesoNave <= 5000)
+            {
+                Console.WriteLine("Listo para el despegue mi capitan.");
+            }
+            else
+            {
+                Diferencia = PesoNave - 5000;
+                Console.WriteLine("Alerta: Se debe descargar " + Diferencia + "kg para poder despegar.");
+            }
+
+        }
+    }
+}
