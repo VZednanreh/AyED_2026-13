2d91ac2b39daee0513a544c82c4eecf842ba1baf
VZednanreh <valencauna@gmail.com>
2026-04-07 16:23:10 -0300

    Add files via upload

---
+++ 13_Cauna_MJ/TP5 Estacionamiento_Chiqui_Tapia.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int TiempoEstacionado;
+
+            int MinutosEstacionados;
+
+            int HorasEstacionadas;
+
+            Double CostoTotal;
+
+            Double IVA = 0.21;
+
+            Double CostoConIVA;
+
+            Double CostoFinal;
+
+            Console.Write("Ingrese los minutos que estuvo estacionado: ");
+
+            TiempoEstacionado = Convert.ToInt32(Console.ReadLine());
+
+            HorasEstacionadas = TiempoEstacionado / 60;
+
+            MinutosEstacionados = TiempoEstacionado % 60;
+
+            CostoTotal = HorasEstacionadas * 6000 + MinutosEstacionados * 150;
+
+            CostoConIVA = CostoTotal * IVA;
+
+            CostoFinal = CostoConIVA + CostoTotal;
+
+            Console.WriteLine("El monto final a pagar es " + CostoFinal + ".");
+
+        }
+    }
+}
