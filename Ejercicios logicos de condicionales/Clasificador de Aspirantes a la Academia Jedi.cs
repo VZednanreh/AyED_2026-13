3b083284d10baff2174b2e756f90e50984f50a16
VZednanreh <valencauna@gmail.com>
2026-04-14 14:47:30 -0300

    Add files via upload

---
+++ Ejercicios logicos de condicionales/Clasificador de Aspirantes a la Academia Jedi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double NivelJedi;
+            Console.Write("Ingrese su nivel de midiclorianos : ");
+            NivelJedi = Convert.ToDouble(Console.ReadLine());
+            if (NivelJedi > 15000)
+            {
+                Console.WriteLine("Potencial de maestro jedi detectado. Avisar al consejo.");
+            }
+            else if(NivelJedi < 15000 & NivelJedi > 5000)
+            {
+                Console.WriteLine("Aceptable para entrenamiento Padawan.");
+            }
+            else
+            {
+                Console.WriteLine("No posee sensibilidad a la Fuerza.");
+            }
+        }
+    }
+}
