9f0badaf89f7527cb8a60807e6cdfcc2203b6215
VZednanreh <valencauna@gmail.com>
2026-04-14 15:57:59 -0300

    Add files via upload

---
+++ Ejercicios logicos de condicionales/Calculadora de Saltos al Hiperespacio.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double CombusNecesario;
+            double faltante;
+            double resto;
+            double Distancia;
+            double Combustible;
+            Console.Write("Ingrese la distancia a recorer: ");
+            Distancia = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese la cantidad de combustible disponible: ");
+            Combustible = Convert.ToDouble(Console.ReadLine());
+            if (Combustible < 0)
+            {
+                Console.Write("No seas tarado, escribime una cantidad no negativa: ");
+                Combustible = Convert.ToDouble(Console.ReadLine());
+                CombusNecesario = Distancia * 12;
+                faltante = CombusNecesario - Combustible;
+                resto = Combustible % CombusNecesario;
+                if (resto <= 0)
+                {
+                    Console.WriteLine("Calculos precisos. Saltando al hiperespacio.");
+                }
+                else
+                {
+                    Console.WriteLine("Peligro: Combustible insuficiente. Faltan " + faltante + " Litros.");
+                }
+            }
+            else
+            {
+                CombusNecesario = Distancia * 12;
+                faltante = CombusNecesario - Combustible;
+                resto = Combustible % CombusNecesario;
+                if (resto <= 0)
+                {
+                    Console.WriteLine("Calculos precisos. Saltando al hiperespacio.");
+                }
+                else
+                {
+                    Console.WriteLine("Peligro: Combustible insuficiente. Faltan " + faltante + " Litros.");
+                }
+            }
+            
+        }
+    }
+}
