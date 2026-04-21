854f5ae49261f4de8c48468e6cfe66c4fa3dbc64
VZednanreh <valencauna@gmail.com>
2026-04-21 16:49:09 -0300

    Add files via upload

---
+++ Ejercicios condicionales 2/SistemaDeBecas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Distancia;
+            Double promedio;
+            Console.Write("Ingrese su promedio de notas: ");
+            promedio = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese su distancia de su casa a la escuela: ");
+            Distancia = Convert.ToDouble(Console.ReadLine());
+            if (promedio > 8 | Distancia > 50)
+            {
+                Console.WriteLine("Beca consedida.");
+            }
+            else
+            {
+                Console.WriteLine("Beca rechazada.");
+            }
+        }
+    }
+}
