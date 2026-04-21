a2ce3e8ce1270fa625105444000ecf3440035e16
VZednanreh <valencauna@gmail.com>
2026-04-21 16:43:39 -0300

    Add files via upload

---
+++ Ejercicios condicionales 2/AumentoSalarial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double antiguedad;
+            Double sueldo;
+            Console.Write("Ingrese su sueldo actual: ");
+            sueldo = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese su antiguedad en la empresa: ");
+            antiguedad = Convert.ToDouble(Console.ReadLine());
+            Double Incremento;
+            if (antiguedad > 10)
+            {
+                Incremento = sueldo * 0.20;
+                Double SueldoNuevo = sueldo + Incremento;
+                Console.WriteLine("Su sueldo nuevo es de " + SueldoNuevo + ".");
+            }
+            else
+            {
+                Incremento = sueldo * 0.05;
+                Double SueldoNuevo = sueldo + Incremento;
+                Console.WriteLine("Su sueldo nuevo es de " + SueldoNuevo + ".");
+            }
+        }
+    }
+}
---
+++ Ejercicios condicionales 2/ConvertorDeSegundos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tiempo;
+            int segundos;
+            int Minutos;
+            Console.Write("Ingrese una cantidad de segundos");
+            tiempo = Convert.ToInt32(Console.ReadLine());
+            if (tiempo > 60)
+            {
+                Minutos = tiempo / 60;
+                segundos = tiempo % 60;
+                Console.WriteLine(Minutos + "." + segundos + " minutos.");
+            }
+            else
+            {
+                Console.WriteLine(tiempo + " segundos.");
+            }
+        }
+    }
+}
