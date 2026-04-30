64be0c8fa517fa74b65be12a5483d7a8a7f9ae18
VZednanreh <valencauna@gmail.com>
2026-04-30 16:48:13 -0300

    Add files via upload

---
+++ Ejercicios condicionales 2/GestióndeInventarioyDesperdicio.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la cantidad de facturas producidas hoy: ");
+            int ProduccionHoy = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese las facturas vendidas: ");
+            Double FacturasVendidas = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese el precio de la docena de facturas: ");
+            Double PrecioDeDocena = Convert.ToDouble(Console.ReadLine());
+            int Docenas = ProduccionHoy / 12;
+            Double FacturasSobrantes = ProduccionHoy - FacturasVendidas;
+            Double RecaudacionHoy = Docenas * PrecioDeDocena;
+            Console.WriteLine("El dinero total recaudado es de " + RecaudacionHoy + ".");
+            Console.WriteLine("Las facturas que sobraron fueron " + FacturasSobrantes + ".");
+            if (FacturasSobrantes <= 6)
+            {
+                Console.WriteLine("Produccion eficiente.");
+            }
+            else if (FacturasSobrantes >= 6)
+            {
+                Console.WriteLine("Alerta: Exceso de produccion.");
+            }
+        }
+    }
+}
