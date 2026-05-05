0830aad37c57ff8617f29a725a7a8373876d9f3b
VZednanreh <valencauna@gmail.com>
2026-05-05 16:57:00 -0300

    Add files via upload

---
+++ Ejercicios de Switch, Contadores, acumuladores y FOR/MenúDeRestaurante.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese uno de los siguientes idiomas:");
+            Console.WriteLine("1 = ingles");
+            Console.WriteLine("2 = Frances");
+            Console.WriteLine("3 = aleman");
+            Console.Write("Ingrese el idioma deseado: ");
+            string opcion = Console.ReadLine(); 
+            switch (opcion)
+            {
+                case "1": Console.WriteLine("Hello."); break;
+                case "2": Console.WriteLine("Salut.");  break;
+                case "3": Console.WriteLine("Hallo.");  break;
+                default: Console.WriteLine("Su eleccion no existe."); break;
+            }
+        }
+    }
+}
---
+++ Ejercicios de Switch, Contadores, acumuladores y FOR/SelectorDeIdioma.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperatura;
+            double contador = 0;
+            for (int x= 1; x <= 7; x++)
+            {
+                Console.Write("Ingrese la temperatura del dia " + x + ".");
+                temperatura = double.Parse(Console.ReadLine());
+                double acumuladorTemp = 0;
+                acumuladorTemp = acumuladorTemp + temperatura;
+                if (temperatura < 0)
+                {
+                    contador++;
+                }
+            }
+            //Console.Write("Ingrese la temperatura del dia " + x + ".");
+            //temperatura = double.Parse(Console.ReadLine());
+            //double acumuladorTemp = 0;
+            //acumuladorTemp = acumuladorTemp + temperatura;
+            //if (temperatura < 0)
+            //{
+            //    contador++;
+            //}
+        }
+    }
+}
