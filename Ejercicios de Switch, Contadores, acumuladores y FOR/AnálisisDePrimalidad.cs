34423712a698edaf311c4f6244bc24487e5f436f
VZednanreh <valencauna@gmail.com>
2026-05-07 15:28:25 -0300

    Add files via upload

---
+++ Ejercicios de Switch, Contadores, acumuladores y FOR/AnálisisDePrimalidad.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            int numero;
+            Console.Write("Ingrese un numero entero positivo porfavor o sos wachin: ");
+            numero = int.Parse(Console.ReadLine());
+            for (int x = 1; x <= numero; x++)
+            {
+                if (numero % x == 0)
+                {
+                    Console.WriteLine(numero + " es divisible por " + x + ".");
+                }
+                else
+                {
+                    Console.WriteLine(numero + " no es divisible por " + x + ".");
+                }
+                contador++;
+            }
+            if (contador == numero)
+            {
+                if (numero % 1 == 0 & numero % numero == 0)
+                {
+                    Console.WriteLine("Es primo.");
+                }
+            }
+        }
+    }
+}
---
+++ Ejercicios de Switch, Contadores, acumuladores y FOR/GestiónDeGastos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double acumuladorGastos = 0;
+            double acumuladorTotalGastos;
+            double gasto;
+            double contador = 0;
+            for (int x = 1; x <= 5; x++)
+            {
+                Console.Write("Ingrese el gasto N°" + x + ": ");
+                gasto = double.Parse(Console.ReadLine());
+                acumuladorGastos = acumuladorGastos + gasto;
+                if (gasto > 500)
+                {
+                    contador++;
+                }
+                acumuladorTotalGastos = acumuladorGastos;
+                if (x == 5)
+                {
+                    Console.WriteLine("la suma de todos los gastos de hoy fueron de " + acumuladorTotalGastos + " y hubo " + contador + " gastos mayores a $500.");
+                }
+                else
+                {
+                    Console.WriteLine("la suma de todos los gastos de hoy por ahora es de " + acumuladorTotalGastos + " y hubo " + contador + " gastos mayores a $500.");
+                }
+            }
+        }
+    }
+}
