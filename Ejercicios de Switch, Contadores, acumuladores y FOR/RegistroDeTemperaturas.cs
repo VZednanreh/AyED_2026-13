6e3a2b40163d94132766bd77e8e45ac48dbbebd8
VZednanreh <valencauna@gmail.com>
2026-05-07 14:06:36 -0300

    Update RegistroDeTemperaturas.cs

--- Ejercicios de Switch, Contadores, acumuladores y FOR/RegistroDeTemperaturas.cs
+++ Ejercicios de Switch, Contadores, acumuladores y FOR/RegistroDeTemperaturas.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,29 +10,22 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         {
-            double acumuladorTemp;
+            double acumuladorTemp = 0;
+            double acumuladorTotalTemp;
             double temperatura;
             double contador = 0;
-            for (int x= 1; x <= 7; x++)
+            for (int x = 1; x <= 7; x++)
             {
-                Console.Write("Ingrese la temperatura del dia " + x + ".");
+                Console.Write("Ingrese la temperatura del dia " + x + ": ");
                 temperatura = double.Parse(Console.ReadLine());
-                acumuladorTemp = 0;
                 acumuladorTemp = acumuladorTemp + temperatura;
                 if (temperatura < 0)
                 {
                     contador++;
                 }
+                acumuladorTotalTemp = acumuladorTemp;
+                Console.WriteLine("la suma de todas las temperaturas de cada dia por ahora es de " + acumuladorTotalTemp + " y hubo " + contador + " dias que la temperatura fue menor a 0°.");
             }
-            Console.WriteLine("la suma de todas las temperaturas de cada dia es de " + acumuladorTemp + " y hubo " + contador +);
-            //Console.Write("Ingrese la temperatura del dia " + x + ".");
-            //temperatura = double.Parse(Console.ReadLine());
-            //double acumuladorTemp = 0;
-            //acumuladorTemp = acumuladorTemp + temperatura;
-            //if (temperatura < 0)
-            //{
-            //    contador++;
-            //}
         }
     }
 }
