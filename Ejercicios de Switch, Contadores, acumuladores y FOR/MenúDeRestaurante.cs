cd6ddf4e6ae1e11d9f82f235b47b6f5dacbfb2f1
VZednanreh <valencauna@gmail.com>
2026-05-07 14:46:36 -0300

    Update MenúDeRestaurante.cs

--- Ejercicios de Switch, Contadores, acumuladores y FOR/MenúDeRestaurante.cs
+++ Ejercicios de Switch, Contadores, acumuladores y FOR/MenúDeRestaurante.cs
@@ -10,18 +10,18 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ingrese uno de los siguientes idiomas:");
-            Console.WriteLine("1 = ingles");
-            Console.WriteLine("2 = Frances");
-            Console.WriteLine("3 = aleman");
-            Console.Write("Ingrese el idioma deseado: ");
+            Console.WriteLine("Ingrese uno de los siguientes pedidos:");
+            Console.WriteLine("1 = Pizza  $11000");
+            Console.WriteLine("2 = Sandwich  $4500");
+            Console.WriteLine("3 = Hamburguesa  $6500");
+            Console.Write("Ingrese el pedido deseado: ");
             string opcion = Console.ReadLine(); 
             switch (opcion)
             {
-                case "1": Console.WriteLine("Hello."); break;
-                case "2": Console.WriteLine("Salut.");  break;
-                case "3": Console.WriteLine("Hallo.");  break;
-                default: Console.WriteLine("Su eleccion no existe."); break;
+                case "1": Console.WriteLine("Su pedido fue la pizza y te sale $11000."); break;
+                case "2": Console.WriteLine("Su pedido fue el sandwich y te sale $4500.");  break;
+                case "3": Console.WriteLine("Su pedido fue la hamburguesa y te sale $6500.");  break;
+                default: Console.WriteLine("Su pedido no existe."); break;
             }
         }
     }
