1326e251309d6eab7afadf3cc93708d82c03c240
VZednanreh <valencauna@gmail.com>
2026-05-21 15:39:41 -0300

    Add files via upload

---
+++ While y Do-While/Tabla de multiplicar personalizada.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero entero positivo: ");
+            int numero = int.Parse(Console.ReadLine());
+            int ciclo = 0;
+            int multiplicacion = 1;
+            do
+            {
+                if (ciclo <= 12)
+                {
+                    multiplicacion = numero * ciclo;
+                    Console.WriteLine(numero + "x" + ciclo + "=" + multiplicacion);
+                    ciclo++;
+                }
+            } while (numero <= 12);
+        }
+    }
+}
