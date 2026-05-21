0528995db17ffcdd5a6bb7a1a9b4e1f282fda0b5
VZednanreh <valencauna@gmail.com>
2026-05-21 16:57:54 -0300

    Add files via upload

---
+++ While y Do-While/nose no lo complete aun.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int promedio;
+            int contador = 1;
+            Console.Write("Ingrese la nota del alumno N°" + contador + ": ");
+            int nota = int.Parse(Console.ReadLine());
+            do
+            {
+                if ()
+                {
+
+                }
+                Console.Write("Ingrese la nota del alumno N°" + contador + ": ");
+                contador++;
+            } while (contador <=5);
+        }
+    }
+}
