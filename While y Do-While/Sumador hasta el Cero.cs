7f30434482913d6cf396659a236d56f5955d6d85
VZednanreh <valencauna@gmail.com>
2026-05-21 14:24:25 -0300

    Add files via upload

---
+++ While y Do-While/Sumador hasta el Cero.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese el dinero a para guardar: ");
+            int numero = int.Parse(Console.ReadLine());
+            int alcancia= 0;
+            do
+            {
+                if (numero > 0)
+                {
+                    alcancia += numero;
+                    Console.Write("ingrese el dinero a para guardar: ");
+                    numero = int.Parse(Console.ReadLine());
+                }
+            } while (numero > 0);
+            Console.WriteLine("El total guardado fue de: " + alcancia);
+        }
+    }
+}
