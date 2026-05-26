c8c08be343bbf5f5fdcf70078bd0392d7e6dc8ee
VZednanreh <valencauna@gmail.com>
2026-05-26 15:51:21 -0300

    Add files via upload

---
+++ While y Do-While/Multiplicación por sumas sucesivas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese 2 numeros para multiplicarse: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el numero para multiplicar " + numero + ": ");
+            int multiplicacion = int.Parse(Console.ReadLine());
+            int acumulador = 0;
+            int contador = 1;
+            while (contador <= multiplicacion)
+            {
+                acumulador += numero;
+                contador++;
+            }
+            int resultado = acumulador;
+            Console.WriteLine(numero + "x" + multiplicacion + "=" + resultado);
+        }
+    }
+}
