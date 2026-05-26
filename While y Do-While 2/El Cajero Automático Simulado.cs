bdde9ffe2829a48abc3c1dd203b46d35e8e767b4
VZednanreh <valencauna@gmail.com>
2026-05-26 16:41:27 -0300

    Add files via upload

---
+++ While y Do-While 2/El Cajero Automático Simulado.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int saldo = 10000;
+            int opcion = 0;
+
+            while (opcion != 4)
+            {
+                Console.WriteLine("1. Depositar dinero");
+                Console.WriteLine("2. Retirar dinero");
+                Console.WriteLine("3. Ver saldo actual");
+                Console.WriteLine("4. Salir");
+                Console.Write("Ingrese su opcion ideal: ");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("Ingrese la cantidad a depositar: ");
+                        int deposito = int.Parse(Console.ReadLine());
+
+                        if (deposito > 0)
+                        {
+                            saldo += deposito;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Cantidad inválida. Debe ser mayor a 0.");
+                        }
+                        break;
+                    case 2:
+                        Console.Write("Ingrese la cantidad a retirar: ");
+                        int retiro = int.Parse(Console.ReadLine());
+
+                        if (retiro > 0)
+                        {
+                            if (retiro <= saldo)
+                            {
+                                saldo -= retiro;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Fondos insuficientes.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Cantidad inválida. Debe ser mayor a 0.");
+                        }
+                        break;
+                    case 3: Console.WriteLine("Su saldo es de " + saldo); break;
+                    case 4: Console.WriteLine("Cerrando..."); break;
+                    default: Console.WriteLine("Su opcion no existe."); break;
+                }
+            }
+        }
+    }
+}
---
+++ While y Do-While 2/El juego de adivina el Número con límites.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroSecreto = 73;
+            int maxIntentos = 5;
+            int intentosUsados = 0;
+            int numero = 0;
+            Console.WriteLine("Tenes " + maxIntentos+ " intentos para adivinar el número secreto.");
+            while (intentosUsados < maxIntentos)
+            {
+                intentosUsados++;
+                Console.Write("Ingresa tu número: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero == numeroSecreto)
+                {
+                    break;
+                }
+
+                if (numero < numeroSecreto)
+                {
+                    Console.WriteLine("El número es mayor.");
+                }
+                else
+                {
+                    Console.WriteLine("El número es menor.");
+                }
+            }
+            if (numero == numeroSecreto)
+            {
+                Console.WriteLine("Felicidades. Ganaste el juego.");
+                Console.WriteLine("Adivinaste el número secreto " + numeroSecreto + " en " + intentosUsados + " intentos.");
+            }
+            else
+            {
+                Console.WriteLine("Perdiste. Te quedaste sin intentos.");
+                Console.WriteLine("El número secreto era: " + numeroSecreto);
+            }
+        }
+    }
+}
