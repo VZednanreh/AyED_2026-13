ebfba65a81d760692457e4b199fb2d53cc6e6ac1
VZednanreh <valencauna@gmail.com>
2026-05-28 11:46:14 -0300

    Update Contador de números positivos, negativos y ceros.cs

--- While y Do-While 2/Contador de números positivos, negativos y ceros.cs
+++ While y Do-While 2/Contador de números positivos, negativos y ceros.cs
@@ -1,37 +1,44 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace ConsoleApplication9
 {
-    static void Main()
+    class Program
     {
-        int positivos = 0;
-        int negativos = 0;
-        int ceros = 0;
-        string entrada = "";
-        Console.WriteLine("Ingresa números continuamente. Para salir, presiona 'X' o 'n'.");
-        while (entrada != "X" && entrada != "x" && entrada != "N" && entrada != "n")
+        static void Main(string[] args)
         {
-            Console.Write("Introduce un número (o 'X'/'n' para salir): ");
-            entrada = Console.ReadLine();
-            if (entrada != "X" && entrada != "x" && entrada != "N" && entrada != "n")
+            int positivos = 0;
+            int negativos = 0;
+            int ceros = 0;
+            string entrada = "";
+            Console.WriteLine("Ingresa números continuamente. Para salir, presiona 'X' o 'n'.");
+            while (entrada != "X" && entrada != "x" && entrada != "N" && entrada != "n")
             {
-                int numero = int.Parse(entrada);
-                if (numero > 0)
+                Console.Write("Introduce un número (o 'X'/'n' para salir): ");
+                entrada = Console.ReadLine();
+                if (entrada != "X" && entrada != "x" && entrada != "N" && entrada != "n")
                 {
-                    positivos++;
-                }
-                else if (numero < 0)
-                {
-                    negativos++;
-                }
-                else
-                {
-                    ceros++;
+                    int numero = int.Parse(entrada);
+                    if (numero > 0)
+                    {
+                        positivos++;
+                    }
+                    else if (numero < 0)
+                    {
+                        negativos++;
+                    }
+                    else
+                    { 
+                        ceros++;
+                    }
                 }
             }
+            Console.WriteLine("Números Positivos: " + positivos);
+            Console.WriteLine("Números Negativos: " + negativos);
+            Console.WriteLine("Cantidad de Ceros: " + ceros);
         }
-        Console.WriteLine("Números Positivos: " + positivos);
-        Console.WriteLine("Números Negativos: " + negativos);
-        Console.WriteLine("Cantidad de Ceros: " + ceros);
     }
 }
