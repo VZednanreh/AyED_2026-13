6bfec01e00f5326463380811ce14ea938119e732
VZednanreh <valencauna@gmail.com>
2026-06-11 14:15:41 -0300

    Add files via upload

---
+++ While y Do-While 3/Filtro estadístico de edades (Censo).cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Edades = 0;
+            int contador = 0;
+            int mayoresDeEdad = 0;
+            int menoresDeEdad = 0;
+            int edad = 0;
+            Console.WriteLine("Ingrese las edades de las personas. Para finalizar ingrese un número negativo.");
+
+            while (edad >= 0)
+            {
+                Console.Write("Ingrese la edad: ");
+                edad = int.Parse(Console.ReadLine());
+                if (edad >= 0)
+                {
+                    Edades += edad;
+                    contador++;
+
+                    if (edad >= 18)
+                    {
+                        mayoresDeEdad++;
+                    }
+                    if (edad < 18)
+                    {
+                        menoresDeEdad++;
+                    }
+                }
+            }
+
+            if (contador > 0)
+            {
+                double promedio = (double)Edades / contador;
+
+                Console.WriteLine("Promedio de edad: " + promedio + "años.");
+                Console.WriteLine("Personas mayores de edad: " + mayoresDeEdad);
+                Console.WriteLine("Personas menores de edad: " + menoresDeEdad);
+            }
+
+            if (contador == 0)
+            {
+                Console.WriteLine("No se registraron edades válidas.");
+            }
+        }
+    }
+}
