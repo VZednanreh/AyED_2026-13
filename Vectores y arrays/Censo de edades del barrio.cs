8e79a4e5598b362331e3185d4a5e980ec91da68a
VZednanreh <valencauna@gmail.com>
2026-08-18 15:42:54 -0300

    Add files via upload

---
+++ Vectores y arrays/Censo de edades del barrio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] edades = new int[12];
+            int menores = 0;
+            int adultosMayores = 0;
+            int intermedios = 0;
+
+            for (int i = 0; i < 12; i++)
+            {
+                Console.Write("Ingrese la edad del vecino N°" + (i + 1) + ": ");
+                edades[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (int edad in edades)
+            {
+                if (edad < 18)
+                {
+                    menores++;
+                }
+                else if (edad >= 65)
+                {
+                    adultosMayores++;
+                }
+                else
+                {
+                    intermedios++;
+                }
+            }
+            Console.WriteLine("Menores de edad: " + menores);
+            Console.WriteLine("Rango intermedio: " + intermedios);
+            Console.WriteLine("Adultos mayores: " + adultosMayores);
+        }
+    }
+}
---
+++ Vectores y arrays/Devolucion de producto por posicion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] productos = new int[10];
+            double dinero = 0;
+            for(int i = 0; i<10; i++)
+            {
+                Console.Write("Ingrese el precio del producto N°" + (i+1) + ": ");
+                productos[i] = int.Parse(Console.ReadLine());
+                
+            }
+            foreach(int plata in productos)
+            {
+                dinero += plata;
+            }
+            
+            Console.WriteLine("Ingrese el numero de la lista del producto que quiere devolver: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("El precio a devolver es de " + productos[numero-1] + ".");
+            double resultado = dinero - productos[numero-1];
+            Console.WriteLine("El nuevo total es de: " + resultado);
+            
+        }
+    }
+}
