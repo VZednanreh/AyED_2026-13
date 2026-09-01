0736cfed7359dfef15780f0962cf339bff4ccb92
VZednanreh <valencauna@gmail.com>
2026-09-01 16:03:48 -0300

    Add files via upload

---
+++ funciones/Potencia.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Potencia();
+        }
+
+        static void Potencia()
+        {
+            int Base;
+            int potencia;
+            Console.Write("Ingrese una base:");
+            Base = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese una potencia:");
+            potencia = int.Parse(Console.ReadLine());
+            int contador = 1;
+            int resultado = Base;
+
+            while (contador < potencia)
+            {
+
+                resultado = resultado * Base;
+
+                contador++;
+            }
+            Console.WriteLine(resultado);
+
+        }
+    }
+}
