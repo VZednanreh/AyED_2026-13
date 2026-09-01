409307d95f32e3d9d068d22afc80e7ca7dd07d47
VZednanreh <valencauna@gmail.com>
2026-09-01 15:59:40 -0300

    Add files via upload

---
+++ funciones/Factorial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Factorial(int n)
+        {
+            int factor = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factor *= i;
+            }
+            Console.WriteLine("La sumatoria dio: " + factor);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un numero entero positivo: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                Factorial(numero);
+            }
+            else
+            {
+                Console.WriteLine("Ingresar un número entero mayor a 0.");
+            }
+        }
+    }
+}
