c43e70bd26f4bcd0fcb114b750cce2387745953d
VZednanreh <valencauna@gmail.com>
2026-09-01 14:38:40 -0300

    Add files via upload

---
+++ funciones/Conteo hasta numero.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        
+        static void ContarHasta(int n)
+        {
+            if (n <= 0)
+            {
+                Console.WriteLine("Ingresa un numero entero positivo.");
+                return;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un número entero positivo: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                ContarHasta(numero);
+            }
+            else
+            {
+                Console.WriteLine("Debes ingresar un número entero mayor a 0.");
+            }
+        }
+    }
+}
---
+++ funciones/cuenta regresiva.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.Write("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+            Console.WriteLine(numero);
+            ContRegre(numero);
+        }
+        static void ContRegre(int num)
+        {
+            while (num != 1)
+            {
+                Console.WriteLine(num - 1);
+                num--;
+            }
+        }
+    }
+}
