5ef0f868db5c5aa8263ba8f2af49d9bf2ed6daf5
VZednanreh <valencauna@gmail.com>
2026-09-03 15:11:40 -0300

    Add files via upload

---
+++ Funciones 2/Ejercicio 1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pepe();
+        }
+        static void pepe()
+        {
+            double acumulador = 0;
+            int pares = 0;
+            int impares = 0;
+
+            Console.Write("Ingrese la cantidad deseada de números: ");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            double[] numeros = new double[cantidad];
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.Write("Ingrese el número " + (i + 1) + ": ");
+                numeros[i] = double.Parse(Console.ReadLine());
+                acumulador += numeros[i];
+                if (numeros[i] % 2 == 0)
+                {
+                    pares++;
+                }
+                else
+                {
+                    impares++;
+                }
+            }
+            double menor = numeros[0];
+            double mayor = numeros[0];
+            foreach (double a in numeros)
+            {
+                if (a < menor)
+                {
+                    menor = a;
+                }
+
+                if (a > mayor)
+                {
+                    mayor = a;
+                }
+            }
+
+            double promedio = acumulador / cantidad;
+
+            Console.WriteLine("Número mayor: " + mayor);
+            Console.WriteLine("Número menor: " + menor);
+            Console.WriteLine("Promedio: " + promedio);
+            Console.WriteLine("Cantidad de pares: " + pares);
+            Console.WriteLine("Cantidad de impares: " + impares);
+        }
+    }
+}
---
+++ Funciones 2/Ejercicio 2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero entero positivo: ");
+            int numero = int.Parse(Console.ReadLine());
+            int sumaDivisores = 0;
+            
+            for (int i = 1; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    sumaDivisores += i;
+                }
+            }
+            
+            if (sumaDivisores == numero && numero > 0)
+            {
+                Console.WriteLine("El numero " + numero + " es un numero perfecto.");
+            }
+            else
+            {
+                Console.WriteLine("El numero " + numero + " no es un numero perfecto.");
+            }
+        }
+    }
+}
